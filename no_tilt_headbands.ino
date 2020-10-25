edfec9c97227f1326183e5361d893f10d483ab9d
Jayda Medina <43220330+jmedina7@users.noreply.github.com>
2020-10-24 21:00:03 -0400

    Update no_tilt_headbands.ino

--- no_tilt_headbands.ino
+++ no_tilt_headbands.ino
@@ -1,16 +1,17 @@
 
 /*
- * Remote codes
- * >>| (SKIP) FFC23D
- * >|| (play) FF02FD
- * 0 FF6897
- * 1 FF30CF
- * 2 FF18E7
- */
+   Remote codes
+   >>| (SKIP) FFC23D
+   >|| (play) FF02FD
+   0 FF6897
+   1 FF30CF
+   2 FF18E7
+*/
 
 #include <IRremote.h>
 #include <LiquidCrystal.h>
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
+char buffer[30];
 
 char const *animals[] = {"snake", "dog", "cat", "elephant", "groundhog", "fox", "wolf"};
 int playerScore = 0;
@@ -32,10 +33,28 @@ void loop() {
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
   }
+  runGame();
   // start text on first row, third column
   for (int i = 0; i < 7; i++) {
     lcd.setCursor(2, 0);
+    lcd.print("                             ");
+    lcd.setCursor(2, 0);
     lcd.print(animals[i]);
+    Serial.println(animals[i]);
     delay(1000);
   }
+  Serial.println("we done");
+}
+
+void runGame() {
+  const size_t n = sizeof(animals) / sizeof(animals[0]);
+  for (size_t i = 0; i < n - 1; i++)
+  {
+    size_t j = random(0, n - i);
+
+    const char *t = animals[i];
+    animals[i] = animals[j];
+    animals[j] = t;
+  }
+
 }
